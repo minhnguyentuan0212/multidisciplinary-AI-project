1c3a1b47f141c81685e360fad54fd4b911a47da9
deedee <minhdatphantran2205@gmail.com>
2024-05-07 09:41:22 +0700

    Upload: face matching

---
+++ Ai-server/app/faceMatching/utils/Singleton.py
@@ -0,0 +1,9 @@
+from threading import Semaphore
+class Singleton(type):
+    _instances = {}
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            with Semaphore(1):
+                if cls not in cls._instances:
+                    cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
+        return cls._instances[cls]
