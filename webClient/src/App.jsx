45d68df47ee76633695c17210584be801f70fd39
Pham Duc Hao <phamduchao2k3@gmail.com>
2024-05-09 13:53:59 +0000

    Change some css

--- webClient/src/App.jsx
+++ webClient/src/App.jsx
@@ -1,49 +1,7 @@
-// import { useEffect, useState } from 'react'
-// import RoomBar from './components/Rooms/RoomsBar'
 import Navigator from './components/Navigator/Navigator'
-// import History from './components/History/History'
-// import Member from './components/Member/Member'
 import './App.css'
-// import Statistics from './components/Statistics/Statistics'
-// import DevicesBar from './components/Devices/DevicesBar'
-// import {getAllRoomsData,getDevicesOfRoom,toggleDevice} from './business/HomePageData'
 import { Outlet } from 'react-router-dom'
 function App() {
-  // const [count, setCount] = useState(0)
-  // const [data, setData] = useState([])
-  // const [selectedRoom, setSelectedRoom] = useState("0")
-  // const [toggleData,setToggleData] = useState(null)
-  // const [devicesData, setDevicesData] = useState({signal:[],devices:[]})
-  // useEffect(()=>{
-  //   const getData = async () => {
-  //     const res = await getAllRoomsData()
-  //     setData(res)
-  //     setSelectedRoom(res[0].room_id)
-  //   }
-  //   getData()
-  // }, []
-  // )
-  // useEffect(()=> {
-  //   const getData = async () => {
-  //     const res = await getDevicesOfRoom(selectedRoom)
-  //     if (selectedRoom != "0") setDevicesData(res);
-  //   }
-  //   getData()
-  // },[selectedRoom,count])
-
-  // useEffect(() => {
-  //   const toggle = async ()=>{
-  //     const res = await toggleDevice(toggleData[0],toggleData[1])
-  //     if (res) {
-  //         setTimeout(()=>{
-  //           setCount((count+1)%2)
-  //         },1000)    
-  //     }
-  //   }
-  //   if (toggleData) {
-  //       toggle()
-  //   }
-  // },[toggleData])
   return (
     <>
     <div className='row'>
@@ -51,22 +9,6 @@ function App() {
         <Navigator></Navigator>
       </div>
       <Outlet></Outlet>
-      {/* <div className='col-6'>
-        <Outlet></Outlet>
-      </div> */}
-      {/* <div className='col-6'>
-        <RoomBar data={[data,selectedRoom,setSelectedRoom]}></RoomBar>
-        <Statistics data={devicesData.signal}></Statistics>
-        <DevicesBar data={[devicesData.devices,setToggleData]}></DevicesBar>
-        </div>
-        <div className='col-3'>
-          <div className='row'>
-            <Member></Member>
-          </div>
-          <div className='row'>
-            <History></History>
-          </div>
-        </div> */}
     </div>
       
     </>
@@ -74,14 +16,3 @@ function App() {
 }
 
 export default App
-// import { Outlet } from "react-router-dom";
-// import Navigator from "./components/Navigator/Navigator";
-// const App = () => {
-//   return (
-//     <>
-//     <Navigator></Navigator>
-//     <Outlet></Outlet>
-//     </>
-//   )
-// }
-// export default App
--- webClient/src/pages/Home.jsx
+++ webClient/src/pages/Home.jsx
@@ -1,69 +1,3 @@
-/* import { useEffect, useState } from 'react'
-import RoomBar from '../components/Rooms/RoomsBar'
-import History from '../components/History/History'
-import Member from '../components/Member/Member'
-import Statistics from '../components/Statistics/Statistics'
-import DevicesBar from '../components/Devices/DevicesBar'
-import {getAllRoomsData,getDevicesOfRoom,toggleDevice} from '../business/HomePageData'
-function Home() {
-  const [count, setCount] = useState(0)
-  const [data, setData] = useState([])
-  const [selectedRoom, setSelectedRoom] = useState("0")
-  const [toggleData,setToggleData] = useState(null)
-  const [devicesData, setDevicesData] = useState({signal:[],devices:[]})
-  useEffect(()=>{
-    const getData = async () => {
-      const res = await getAllRoomsData()
-      setData(res)
-      setSelectedRoom(res[0].room_id)
-    }
-    getData()
-  }, []
-  )
-  useEffect(()=> {
-    const getData = async () => {
-      const res = await getDevicesOfRoom(selectedRoom)
-      if (selectedRoom != "0") setDevicesData(res);
-    }
-    getData()
-  },[selectedRoom,count])
-
-  useEffect(() => {
-    const toggle = async ()=>{
-      const res = await toggleDevice(toggleData[0],toggleData[1])
-      if (res) {
-          setTimeout(()=>{
-            setCount((count+1)%2)
-          },1000)    
-      }
-    }
-    if (toggleData) {
-        toggle()
-    }
-  },[toggleData])
-  return (
-    <>
-        <div className='col-6'>
-        <RoomBar data={[data,selectedRoom,setSelectedRoom]}></RoomBar>
-        <Statistics data={devicesData.signal}></Statistics>
-        <DevicesBar data={[devicesData.devices,setToggleData]}></DevicesBar>
-        </div>
-        <div className='col-3'>
-          <div className='row'>
-            <Member></Member>
-          </div>
-          <div className='row'>
-            <History></History>
-          </div>
-        </div>
-      
-    </>
-  )
-}
-
-export default Home
- */
-
 import { useEffect, useState } from 'react'
 import RoomBar from '../components/Rooms/RoomsBar'
 import History from '../components/History/History'
@@ -101,8 +35,10 @@ function Home() {
   )
   useEffect(()=> {
     const getData = async () => {
-      const res = await getDevicesOfRoom(selectedRoom)
-      if (selectedRoom != "0") setDevicesData(res);
+      if (selectedRoom != "0") {
+        const res = await getDevicesOfRoom(selectedRoom)
+        setDevicesData(res);
+      }
     }
     getData()
   },[selectedRoom,count])
